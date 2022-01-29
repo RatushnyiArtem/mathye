81a866c3ec75d03250a69ab9897eb72ef52c9bec
RatushnyiArtem <92036528+RatushnyiArtem@users.noreply.github.com>
2022-01-29 22:01:54 +0100

    math

---
+++ main.js
@@ -0,0 +1,226 @@
+// Beginer 1
+// let a = prompt('Введите сторону квадрата')
+// let b = 4*a
+// alert(b)
+// console.log(b)
+// Beginer 2
+// let c = prompt('Введите сторону квадрата')
+// let f = c**2
+// alert(f)
+// console.log(f)
+// Beginer 25
+// let x = Number(prompt('Введите x'))
+// let y = 3*(x**6) - 6*(x**2) - 7
+// alert(y)
+// console.log(y)
+// Beginer 3
+// let a = Number(prompt('Введите первую сторону прямоугольника'))
+// let b = Number(prompt('Введите вторую сторону прямоугольника'))
+// let s = a*b
+// alert('Площадь прямоугольника равна ' + s)
+// console.log(s)
+// let p = 2*(a + b)
+// alert('Периметр прямоугольника равен ' + p)
+// console.log(p)
+// Beginer 4
+// let d = Number(prompt('Введите диаметр окружности'))
+// let p = 3.14
+// let l = p*d
+// alert('Длина окружности равна ' + l)
+// console.log(l)
+// Beginer 5
+// let a = Number(prompt('Введите длину ребра'))
+// let v = a**3
+// console.log(v)
+// alert('Объем куба равен ' + v)
+// let s = 6*(a**2)
+// alert('Площадь поверхности равна ' + s)
+// console.log(s)
+// Beginer 6
+// let a = Number(prompt('Введите первую длину параллелепипеда'))
+// let b = Number(prompt('Введите первую длину параллелепипеда'))
+// let c = Number(prompt('Введите первую длину параллелепипеда'))
+// let v = a*b*c
+// alert('Объем параллелепипеда равен ' + v)
+// let s = 2*((a*b)+(b*c)+(a*c))
+// alert('Площадь поверхности равна ' + s)
+// console.log(s)
+// Beginer 7
+// let r = Number(prompt('Введите радиус круга'))
+// let p = 3.14
+// let l = 2*p*r
+// alert('Длина окружности равна ' + l)
+// console.log(l)
+// let s = p*(r**2)
+// alert('Площадь окружности равна ' + s)
+// console.log(s)
+// Beginer 8
+// let a = Number(prompt('Введите первое число'))
+// let b = Number(prompt('Введите второе число'))
+// let ar = (a+b)/2
+// alert('Среднее арифмитическое равно ' + ar)
+// console.log(ar)
+// Beginer 9
+// let a = Number(prompt('Введите первое число'))
+// let b = Number(prompt('Введите второе число'))
+// let c = (a*b)**0.5
+// alert('Среднее геометрическое равно ' + c)
+// console.log(c)
+// Beginer 10
+// let a = Number(prompt('Введите первое ненулевое число'))
+// let b = Number(prompt('Введите второе ненулевое число'))
+//     c = a + b
+// alert('Сумма этих чисел = ' + c)
+// console.log(c)
+//     r = a - b
+// alert('Разница этих чисел = ' + r)
+// console.log(r)
+//     p = a*b
+// alert('Их произведение равно = ' + p)
+// console.log(p)
+//     f = a**2/b**2
+// alert('Частное их квадратов = ' + f)
+// Beginer 11
+// let a = Number(prompt('Введите первое ненулевое число'))
+// let b = Number(prompt('Введите второе ненулевое число'))
+//     c = a + b
+// alert('Сумма этих чисел = ' + c)
+// console.log(c)
+//     r = a - b
+// alert('Разница этих чисел = ' + r)
+// console.log(r)
+//     p = a*b
+// alert('Их произведение равно = ' + p)
+// console.log(p)
+// Math.abs(a);
+// Math.abs(b);
+//     f = a/b
+// alert('Частное их модулей = ' + f)
+// Beginer 12
+// let a = Number(prompt('Введите первый катет треугольника'))
+// let b = Number(prompt('Введите второй катет треугольника'))
+// let e = (a**2+b**2)**0.5
+// let k = a + b + e
+// alert('Гипотенуза треугольника равна = ' + e )
+// console.log(e)
+// alert('Периметр = ' + k)
+// console.log(k)
+// Beginer 13
+// let r1 = +prompt('Введите первый радиус')
+// let r2 = +prompt('Введите второй радиус')
+// let s1 = 3.14*(r1)**2
+// alert('Площадь первого круга равна = ' + s1)
+// console.log(s1)
+// let s2 = 3.14*(r2)**2
+// alert('Площадь второго круга равна = ' + s2)
+// console.log(s2)
+// let s3 = s1-s2
+// if (r1>r2) {
+//     alert('Площадь кольца равна ' + s3)
+//     console.log(s3)
+// } else{
+//     alert('Условие заданно не верно')
+// }
+// Beginer 14
+// let l = +prompt('Введите длину окружности')
+// let r = l/2*3.14
+// alert('Радиус окружности равен = ' + r)
+// console.log(r)
+// let s = 3.14*(r)**2
+// alert('Площадь окружности равна = ' + s)
+// console.log(s)
+// Beginer 15
+// let s = +prompt('Введите площадь окружности')
+//     d = (s/(4*3.14))**0.5
+//     l = 3.14*d
+// alert('Диаметр окружности равен = ' + d)
+// console.log(d)
+// alert('Длина окружности равна = ' + l)
+// console.log(l)
+// Beginer 16
+// let x1 = +prompt('Введите координаты первой точки')
+// let x2 = +prompt('Введите координаты второй точки')
+//     if (x2>x1) {
+//         let x = x2 - x1
+//         alert('Растояние между точками равно = ' + x)
+//         console.log(x)
+//     } else if (x2<x1) {
+//         let x = Math.abs(x2 - x1)
+//         alert('Растояние между точками равно = ' + x)
+//         console.log(x)
+//     } else {
+//         let x = x2 - x1
+//         alert('Растояние между точками равно = ' + x)
+//         console.log(x)
+//     }
+// Beginer 17
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// let c = +prompt('Введите третье число')
+// let f = a + c
+// let k = b + c
+// let s = f + k
+// alert('Отрезок AC равен = ' + f)
+// console.log(f)
+// alert('Отрезок BC равен = ' + k)
+// console.log(k)
+// alert('Сума отрезков равна = ' + s)
+// console.log(s)
+// Beginer 18
+// let a = +prompt('Введите первое число')
+// let b = +prompt('Введите второе число')
+// let c = +prompt('Введите третье число')
+// let f = a + c
+// let k = b + c
+// let s = f * k
+// alert('Отрезок AC равен = ' + f)
+// console.log(f)
+// alert('Отрезок BC равен = ' + k)
+// console.log(k)
+// alert('Произведение отрезков равно = ' + s)
+// console.log(s)
+// Beginer 19
+// let x1 = +prompt('Введите первую точку координат')
+// let y1 = +prompt('Введите первую точку координат')
+// let x2 = +prompt('Введите вторую точку координат')
+// let y2 = +prompt('Введите вторую точку координат')
+// let f = ((x1 - x2)**2)**0.5
+// let d = ((y1 - y2)**2)**0.5
+// let p = 2*(f+d)
+// let s = f*d
+// alert('Площадь прямоугольника равна = ' + s)
+// alert('Периметр прямоугольника равен = ' + p)
+// console.log(p)
+// console.log(s)
+// Beginer 25
+// let x = +prompt('Введите значение x')
+// let y = 3*(x**6)-6*(x**2)-7
+// alert('Корень уравнения = ' + y)
+// console.log(y)
+// Beginer 26
+// let x = +prompt('Введите значение x')
+// let y = 4*((x-3)**6)-7*((x-3)**3)+2
+// alert('Корень уравнения = ' + y)
+// console.log(y)
+// Beginer 27
+// let a = +prompt('Введите число A')
+// let a2 = a**2
+// let a4 = a2**2
+// let a8 = a4**2
+// alert('Возведённое во вторую степень равно = ' + a2)
+// alert('Возведённое в четвертую степень равно = ' + a4)
+// alert('Возведённое в восьмую степень равно = ' + a8)
+// console.log(a2, a4, a8)
+// Beginer 28 
+// let a = +prompt("Введите число А");
+// let a2 = a**2;
+// let a3 = a**3;
+// let a5 = a**5;
+// let a10 = a**10;
+// let a15 = a**15;
+// alert('Возведённое во вторую степень равно = ' + a2)
+// alert('Возведённое в третью степень равно = ' + a3)
+// alert('Возведённое в пятую степень равно = ' + a5)
+// alert('Возведённое в десятую степень равно = ' + a10)
+// alert('Возведённое в пятнадцатую степень равно = ' + a15)
+// console.log(a2, a3, a5, a10, a15)
